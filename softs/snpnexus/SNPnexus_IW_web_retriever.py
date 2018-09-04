f0781a51facb460ae21905744f0e03848448481f
andronekomimi <lemacon.audrey@gmail.com>
2018-09-03 20:55:38 -0400

    python script for SNPnexus data retrieval

---
+++ softs/snpnexus/SNPnexus_IW_web_retriever.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3.5
+
+import re
+import csv
+import sys
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+
+
+def write_csv(filepath, headers, data):
+	with open(filepath, 'w') as f:
+		writer = csv.DictWriter(f, fieldnames=headers)
+		writer.writeheader()
+		for d in data:
+			writer.writerow(d)
+
+def parse_table(table_name, table):
+	search = iter(table.select("tr"))
+	data = []
+	headers = [th.text.lower().replace(' ', '').replace('-', '').replace('(', '').replace(')','') for th in next(search).find_all('td')]
+	for tr in search:
+		data.append(dict(zip(headers, [td.text for td in tr.find_all('td')])))
+	write_csv(outputs[table_name], headers, data)
+
+if __name__ == "__main__":
+
+	# args
+	args = sys.argv
+	assert len(args) == 5
+	snp_file = args[1]
+	output_integrative_score_known = args[2]
+	output_integrative_score_novel = args[3]
+	max_retry = int(args[4]) # minutes
+
+	# file
+	files = {'region_file': open(snp_file, 'rb')}
+
+	# map
+	outputs = {
+		'novel': output_integrative_score_novel,
+		'known': output_integrative_score_known
+	}
+
+	# html payload
+	payload = {
+	    # 'email': 'regis.ongaro-carcy.1@ulaval.ca',
+	    'ensembl': 'ensembl',
+	    'ncsnp': 'ncsnp',
+	    'eigen': 'eigen',
+	    'fathmm': 'fathmm',
+	    'gwava': 'gwava',
+	    'deepsea': 'deepsea',
+	    'funseq2': 'funseq2',
+	    'remm': 'remm',
+	    'pipeline': 'all_var'
+	}
+
+	# For hg19 non-coding annotation
+	url = 'http://snp-nexus.org/cgi-bin/snp/s6_nc.cgi'
+	r = requests.post(url=url, data=payload, files=files)
+	result = r.text
+
+	# get link
+	link = None
+	if result:
+	    r = re.compile(r'http://snp-nexus\.org/test/snpnexus_[0-9]+/results\.html')
+	    m = r.search(result)
+	    if m:
+	        link = m.group(0)
+
+	# wait for results link
+	html_data = None
+	if link is not None:
+		for i in range(max_retry):
+			print("Next try: {} min...".format(i))
+			sleep(60)
+			r = requests.get(url=link)
+			parsed_html = BeautifulSoup(r.text, "lxml")
+			_table = parsed_html.body.find('table', attrs={'class':'sofT2'})
+			if _table:
+				html_data = BeautifulSoup(r.text, "lxml")
+				break
+
+	# # TEST PURPOSE
+	# with open('./resultat_test.html') as f:
+	# 	html_data = BeautifulSoup(f.read(-1), "lxml")
+
+	# parse & csv-ishify result tables
+	if html_data:
+		tables = iter(html_data.find_all('table', attrs={'id':'result-table'})[-2:])
+		parse_table('known', next(tables))
+		parse_table('novel', next(tables))
+
+	else:
+		print('NO DATA')
+
+
+	
